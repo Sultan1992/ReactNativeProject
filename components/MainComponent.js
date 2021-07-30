2c6a9e626a64bd27b5b45f94cfc1bef3b62bb1fa
sultan <sultanfeto@gmail.com>
2021-07-30 14:30:05 -0500

    Forms

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -15,6 +15,7 @@ import { connect } from 'react-redux';
 import { fetchCampsites, fetchComments, fetchPromotions,
     fetchPartners
 } from '../redux/ActionCreators';
+import Reservation from './ReservationComponent';
     
 
 const mapDispatchToProps = {
@@ -23,6 +24,29 @@ const mapDispatchToProps = {
     fetchPromotions,
     fetchPartners
 };
+
+const ReservationNavigator = createStackNavigator(
+    {
+        Reservation: { screen: Reservation }
+    },
+    {
+        defaultNavigationOptions: ({navigation}) => ({
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon
+                name='tree'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })
+    }
+);
 
 const DirectoryNavigator = createStackNavigator(
     {
@@ -160,7 +184,21 @@ const MainNavigator = createDrawerNavigator(
                             />
                         )
                     }
-                },
+        },
+        Reservation: {
+            screen: ReservationNavigator,
+            navigationOptions: {
+                drawerLabel: 'Reserve Campsite',
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name='tree'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        },
                 About: {
                     screen: AboutNavigator,
                     navigationOptions: {
